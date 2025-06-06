5420e7f820801fd9d2fd8cb450ab681f47de1d56
Ms-10182 <22bcs10182@cuchd.in>
2025-06-06 23:25:21 +0530

    .

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+
+// Configure CORS to allow your frontend
+app.use(cors({
+  origin: "*",
+  credentials: true,
+  optionsSuccessStatus: 200
+}));
+
+app.use(cookieParser());
+
+import healthRoute from "./routes/healthCheck.route.js";
+import userRoute from "./routes/user.route.js";
+import listRoute from "./routes/list.route.js";
+import taskRoute from "./routes/task.route.js";
+import { errorHandler } from "./utils/errorHandler.js";
+
+app.use("/api/v1/healthCheck", healthRoute);
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/lists", listRoute);
+app.use("/api/v1/tasks", taskRoute);
+
+// Error handler middleware (must be last)
+app.use(errorHandler);
+
+export { app };
